bd1540673e036439aaac438c5ded0c778ef86e0e
dragunwf <plarisanmarc2@gmail.com>
2024-03-03 19:25:15 +0800

    Update CurrencyConverter.java

--- app/src/main/java/com/example/currencyconverter/CurrencyConverter.java
+++ app/src/main/java/com/example/currencyconverter/CurrencyConverter.java
@@ -3,6 +3,10 @@ package com.example.currencyconverter;
 import java.util.HashMap;
 public class CurrencyConverter {
     private static HashMap<String, Integer> currencies = new HashMap<>();
+    /*
+        These are hard coded values. In an actual currency converter,
+        It is best to use an API to get reliable values.
+    */
 
     public static boolean isValidCurrency(String currency) {
         for (String key : currencies.keySet()) {
