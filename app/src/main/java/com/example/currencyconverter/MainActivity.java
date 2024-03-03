c3ee98de13c1d48ed445de15682c90327b565c08
dragunwf <plarisanmarc2@gmail.com>
2024-03-03 18:07:08 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class MainActivity extends AppCompatActivity {
+    TextInputEditText amountTextInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        amountTextInput = findViewById(R.id.amountTextInput);
+
     }
 }
