43cac50a90cfeacf09f42ae93e700947f32bc4fa
dragunwf <plarisanmarc2@gmail.com>
2024-03-03 18:45:01 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.currencyconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
-    TextInputEditText amountTextInput;
+    private TextInputEditText amountTextInput;
+    private TextInputEditText currencyTypeTextInput;
+    private TextView moneyText;
+    private Button convertButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         amountTextInput = findViewById(R.id.amountTextInput);
+        currencyTypeTextInput = findViewById(R.id.currencyTypeTextInput);
+        moneyText = findViewById(R.id.moneyText);
+        convertButton = findViewById(R.id.convertButton);
 
+        setButtons();
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void setButtons() {
+        convertButton.setOnClickListener(v -> {
+            if (!isValidAmountInput()) {
+                moneyText.setText("Amount text input is invalid! Add only digits please.");
+                return;
+            }
+            moneyText.setText("Input is valid!");
+        });
+    }
+
+    private boolean isValidAmountInput() {
+        String text = String.valueOf(amountTextInput.getText());
+        for (int i = 0, n = text.length(); i < n; i++) {
+            char character = text.charAt(i);
+            if (!(character >= 48 && character <= 57)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
