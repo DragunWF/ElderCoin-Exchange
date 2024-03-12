10870f006cf6d7ac484ec1fbe58b42928e55be0f
dragunwf <plarisanmarc2@gmail.com>
2024-03-12 21:26:08 +0800

    Update GuideActivity.java

--- app/src/main/java/com/example/currencyconverter/GuideActivity.java
+++ app/src/main/java/com/example/currencyconverter/GuideActivity.java
@@ -2,12 +2,17 @@ package com.example.currencyconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GuideActivity extends AppCompatActivity {
+    private TextView currenciesText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+
+        currenciesText = findViewById(R.id.currenciesText);
+        currenciesText.setText(CurrencyConverter.getCurrenciesText());
     }
 }
