14c32f23fdeb8f5892abe80b2e543ad57ab8823e
dragunwf <plarisanmarc2@gmail.com>
2024-03-12 22:18:15 +0800

    Update GuideActivity.java

--- app/src/main/java/com/example/currencyconverter/GuideActivity.java
+++ app/src/main/java/com/example/currencyconverter/GuideActivity.java
@@ -1,11 +1,16 @@
 package com.example.currencyconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Button;
+import android.util.Log;
 
 public class GuideActivity extends AppCompatActivity {
     private TextView currenciesText;
+    private Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +18,17 @@ public class GuideActivity extends AppCompatActivity {
         setContentView(R.layout.activity_guide);
 
         currenciesText = findViewById(R.id.currenciesText);
+        backButton = findViewById(R.id.backButton);
+        Log.d("DEBUG:", CurrencyConverter.getCurrenciesText());
+
         currenciesText.setText(CurrencyConverter.getCurrenciesText());
+        setButtons();
+    }
+
+    private void setButtons() {
+        backButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
